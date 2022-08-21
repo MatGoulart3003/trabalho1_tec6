0b7df04eb60a7951326515cdf54bb2280dcaf1d3
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2022-08-21 14:44:56 -0300

    quarto commit, adicionando pasta config com as rotas e adicionando delete

--- trab1_tp6/index.js
+++ trab1_tp6/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const routes = require('./config/routes')
 
 const app = express()
 
@@ -9,34 +10,7 @@ app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.json())
 app.use(cors()) 
-
-let db = [
-
-    { '1': {nome:'Cliente 1', idade: '20'}},
-    { '2': {nome:'Cliente 3', idade: '22'}},
-    { '3': {nome:'Cliente 3', idade: '23'}}
-
-]
-
-// Buscar Dados
-app.get('/', (req, res) =>{
-    return res.json({db})
-})
-
-// Inserir dados
-app.post('/add' , (req, res) =>{
-    const body = req.body
-
-    if(!body){
-        return res.status(400).end()
-    }
-
-    db.push(body)
-    return res.json(body)
-
-})
-
-
+app.use(routes)
 
 
 app.listen(21262, () => {
