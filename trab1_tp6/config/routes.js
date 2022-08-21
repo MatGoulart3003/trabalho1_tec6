9dc3f2fa389cbf74b24c5e0c3b2674861c32e2ea
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2022-08-21 17:10:34 -0300

    começando a logica

--- trab1_tp6/config/routes.js
+++ trab1_tp6/config/routes.js
@@ -1,10 +1,9 @@
 const express = require('express')
+const moment = require('moment')
 const routes = express.Router()
 
 let db = [
 
-   {ano:'2022', sem: '4', dia:'1'}
-    
 
 ]
 
@@ -14,41 +13,24 @@ routes.get('/', (req, res) =>{
 })
 
 // Inserir dados
-routes.post('/api/v1/classes/:ano/:sem/:dia' , (req, res) =>{
+routes.post('/api/v1/classes/' , (req, res) =>{
     const body = req.body
-    const ano = req.params.ano
-    const sem = req.params.sem
-    const dia = req.params.dia
-
     if(!body){
         return res.status(400).end()
     }
+    validBody(body)
 
-    if(ano < 2020 || sem > 2 || dia > 5 || sem < 1 || dia < 1){
-        console.log("deu ruim rapeize")
-        return res.status(404).end
-    }
-    
-    body.ano = ano
-    body.sem = sem
-    body.dia = dia
-    
     db.push(body)
     return res.json(body)
 
 })
 
-routes.delete('/:id', (req,res) =>{
-    const id = req.params.id
-
-    let newDB = db.filter(item =>{
-        if(!item[id])
-            return item
-    })
-
-    db = newDB
-
-    return res.send(newDB)
-})
+const validBody = (body) => {
+    const {ano, semestre, dias_da_sem} = body
+    
+    if(ano == undefined || semestre == undefined || dias_da_sem == undefined){
+        return res.status(404).end
+    }
+}
 
 module.exports = routes
