18365b2ba829a3098dc96b945c5637e8b03fae33
marciofaria9 <m.faria.jr@icloud.com>
2022-09-04 19:22:50 -0300

    mais validacao

--- trab1_tp6/config/routes.js
+++ trab1_tp6/config/routes.js
@@ -9,64 +9,69 @@ const mensagemErro = "Requisição com Erro"
 let db = []
 
 // Buscar Dados
-routes.get('/', (req, res) =>{
-    return res.json({db})
+routes.get('/', (req, res) => {
+    return res.json({ db })
 })
 
 // Inserir dados
-routes.post('/api/v1/classes/' , (req, res) =>{
+routes.post('/api/v1/classes/', (req, res) => {
     const body = req.body
-    if(!body){
+    if (!body) {
         return res.status(404).send(mensagemErro)
     }
-    if (validBody(body)){
+    if (validBody(body)) {
         return res.status(404).send(mensagemErro)
     }
 
-    const {ano, semestre, dias_da_sem} = body
-    
-    if(semestre === 1){    
-        
+    const { ano, semestre, dias_da_sem } = body
+
+    if (semestre === 1) {
+
         let data = moment(`${ano}-02-01`).format("DD/MM/YYYY")
 
-        
-       
-    }else{
-        
+
+
+    } else {
+
         let data = moment(`${ano}-08-01`).format("DD/MM/YYYY")
-       
+
     }
-    
+
     db.push(body)
     return res.json(body)
 
 })
 
-const validBody = (body) => {
-    const {ano, semestre, dias_da_sem} = body
+    const validBody = (body) => {
+    const { ano, semestre, dias_da_sem } = body
     let isntValid = true
-   
-    if(ano === undefined || semestre === undefined || dias_da_sem === undefined){        
-         return isntValid
+
+
+    if (new Set(dias_da_sem).size !== dias_da_sem.length) {
+        return isntValid
     }
-    if(ano > 2029 || ano < 2020 || semestre > 2 || semestre < 1){
+
+    if (ano === undefined || semestre === undefined || dias_da_sem === undefined) {
+        return isntValid
+    }
+    if (ano > 2029 || ano < 2020 || semestre > 2 || semestre < 1) {
         return isntValid
     }
     if (!Array.isArray(dias_da_sem)) {
         return isntValid
     }
-    if(dias_da_sem.length < 1 || dias_da_sem.length > 5){
+    if (dias_da_sem.length < 1 || dias_da_sem.length > 5) {
         return isntValid
-    }if(dias_da_sem){
+    } if (dias_da_sem) {
         for (let i = 0; i < dias_da_sem.length; i++) {
             let dia = dias_da_sem[i]
-            if (dia < 1 || dia > 5){
+            if (dia < 1 || dia > 5) {
                 return isntValid
-            }            
+            }
         }
-        
+
     }
-           
+
 }
 
 
